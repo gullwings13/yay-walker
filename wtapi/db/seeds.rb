5c18f69e5cae9999b76682ed50e3d4e06f0c57cb
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-03-06 17:20:07 -0500

    updated seedfile. reset db works.

--- wtapi/db/seeds.rb
+++ wtapi/db/seeds.rb
@@ -10,11 +10,11 @@ require 'faker'
 
 user1 = User.create(email:"trev@trev.com", password:"password")
 
-tour1u1 = Tour.create(name:Faker::Hipster.sentence(word_count: 3), description: Faker::Hipster.paragraphs(number: 1), img_url: Faker::Placeholdit.image)
+tour1u1 = Tour.create(name:Faker::Hipster.sentence(word_count: 3), description: Faker::Hipster.paragraphs(number: 1), img_url: 'https://source.unsplash.com/random')
 
-point1u1 = Point.create(lat: 40.740019, long: -73.990045, order: 1, text: Faker::Hipster.paragraphs(number: 1), picture_url: Faker::Placeholdit.image)
-point2u1 = Point.create(lat: 40.740058, long: -73.989581, order: 1, text: Faker::Hipster.paragraphs(number: 1), picture_url: Faker::Placeholdit.image)
-point3u1 = Point.create(lat: 40.740574, long: -73.989440, order: 1, text: Faker::Hipster.paragraphs(number: 1), picture_url: Faker::Placeholdit.image)
+point1u1 = Point.create(lat: 40.740019, long: -73.990045, order: 1, text: Faker::Hipster.paragraphs(number: 1), picture_url: 'https://source.unsplash.com/random')
+point2u1 = Point.create(lat: 40.740058, long: -73.989581, order: 1, text: Faker::Hipster.paragraphs(number: 1), picture_url: 'https://source.unsplash.com/random')
+point3u1 = Point.create(lat: 40.740574, long: -73.989440, order: 1, text: Faker::Hipster.paragraphs(number: 1), picture_url: 'https://source.unsplash.com/random')
 
 tour1u1.points.push(point1u1)
 tour1u1.points.push(point2u1)
@@ -25,10 +25,10 @@ user1.tours.push(tour1u1)
 10.times do
     user = User.create(email:Faker::Internet.email, password: Faker::Internet.password(min_length: 10, max_length: 20))
     5.times do
-        tour = Tour.create(name:Faker::Hipster.sentence(word_count: 3), description: Faker::Hipster.paragraphs(number: 1), img_url: Faker::Placeholdit.image)
+        tour = Tour.create(name:Faker::Hipster.sentence(word_count: 3), description: Faker::Hipster.paragraphs(number: 1), img_url: 'https://source.unsplash.com/random')
 
         3.times do
-            point1 = Point.create(lat: Faker::Address.latitude, long: Faker::Address.longitude, order: 1, text: Faker::Hipster.paragraphs(number: 1), picture_url: Faker::Placeholdit.image)
+            point1 = Point.create(lat: Faker::Address.latitude, long: Faker::Address.longitude, order: 1, text: Faker::Hipster.paragraphs(number: 1), picture_url: 'https://source.unsplash.com/random')
 
             tour.points.push(point1)
         end
