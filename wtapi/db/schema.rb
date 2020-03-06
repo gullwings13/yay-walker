195e758af8b067261a3ff49490156f6732aab4d1
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-03-06 14:51:54 -0500

    Ran migrations, updated gitignore

--- wtapi/db/schema.rb
+++ wtapi/db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_171934) do
+ActiveRecord::Schema.define(version: 2020_03_06_191721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "points", force: :cascade do |t|
+    t.float "lat"
+    t.float "long"
+    t.integer "order"
+    t.string "text"
+    t.string "picture_url"
+    t.bigint "tour_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tour_id"], name: "index_points_on_tour_id"
+  end
+
+  create_table "tours", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "img_url"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tours_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +52,6 @@ ActiveRecord::Schema.define(version: 2020_03_06_171934) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "points", "tours"
+  add_foreign_key "tours", "users"
 end
